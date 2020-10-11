df2dd02b4f834e9ff74ba7172d1dcab74a8895e2
matumoto76i <34masamasa52@gmail.com>
2020-10-11 23:19:23 +0900

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # devise_for :users, controllers: {
+  #   registrations: 'users/registrations',
+  # }
+
+  root to: 'posts#index'
+  
+  resources :posts, only: [:index]
+  # resources :users, only: [:index, :show]
+  # end
+end
