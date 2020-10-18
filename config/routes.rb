67f0fe5233e83004b2ad25e72bf2d5541bb83805
matumoto76i <34masamasa52@gmail.com>
2020-10-18 20:50:32 +0900

    マイページの作成に必要なファイルを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  # devise_for :users, controllers: {
-  #   registrations: 'users/registrations',
-  # }
 
   root to: 'posts#index'
   
   resources :posts, only: [:index]
-  # resources :users, only: [:index, :show]
-  # end
+  resources :users, only: [:index, :show]
+  end
 end
