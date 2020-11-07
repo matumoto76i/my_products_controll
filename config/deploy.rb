772a5f7f14af4e1ba6e87a80a21ce285e3eb41b4
matumoto76i <34masamasa52@gmail.com>
2020-11-07 16:37:47 +0900

    deploy.rbの記述を変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
 # secrets.yml用のシンボリックリンクを追加
-set :linked_files, %w{ config/credentials.yml.enc }
+set :linked_files, %w{ config/master.key }
 
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
@@ -35,13 +35,13 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 
-  desc 'upload credentials.yml.enc'
+  desc 'upload master.key'
   task :upload do
     on roles(:app) do |host|
       if test "[ ! -d #{shared_path}/config ]"
         execute "mkdir -p #{shared_path}/config"
       end
-      upload!('config/credentials.yml.enc', "#{shared_path}/config/credentials.yml.enc")
+      upload!('config/master.key', "#{shared_path}/config/master.key")
     end
   end
   before :starting, 'deploy:upload'
