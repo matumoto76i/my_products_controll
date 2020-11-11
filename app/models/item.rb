01dbb5b133e015df9de9634fe01a634e989cc716
matumoto76i <34masamasa52@gmail.com>
2020-11-11 23:02:06 +0900

    validationの設定及びビューの調整

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,10 @@ class Item < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
+
   validates :images, presence: true
+  validates :name, presence: true, length: {maximum: 40}
+  validates :quantity, presence: true
 
   def self.search(search)
     return Item.all unless search
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true, uniqueness: true
+  validates :nickname, presence: true, length: { maximum: 8 }, uniqueness: true
+
+  # パスワードのバリデーション(半角英数字のみ入力を許可)
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
 
   has_many :items
   has_many :comments
